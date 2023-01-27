ed52c08cf832020f8c505a3de7bac26bf9af05da
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:57 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -7,7 +7,7 @@
 
 namespace Aurora\Modules\MailMoveMessagesBetweenAccountsWebclient;
 
-use \Aurora\Modules\Mail;
+use Aurora\Modules\Mail;
 
 /**
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
@@ -18,88 +18,95 @@ use \Aurora\Modules\Mail;
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	public function init() {}
+    public function init()
+    {
+    }
 
-	/**
-	 * Obtains list of module settings.
-	 * @return array
-	 */
-	public function GetSettings()
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+    /**
+     * Obtains list of module settings.
+     * @return array
+     */
+    public function GetSettings()
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
 
-		return [
-			'NumberOfRecordsInHistory' => $this->getConfig('NumberOfRecordsInHistory', 3)
-		];
-	}
+        return [
+            'NumberOfRecordsInHistory' => $this->getConfig('NumberOfRecordsInHistory', 3)
+        ];
+    }
 
-	public function MoveMessages($AccountID, $ToAccountID, $Folder, $ToFolder, $Uids)
-	{
-		$mResult = false;
+    public function MoveMessages($AccountID, $ToAccountID, $Folder, $ToFolder, $Uids)
+    {
+        $mResult = false;
 
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
 
-		$aUids = \Aurora\System\Utils::ExplodeIntUids((string) $Uids);
+        $aUids = \Aurora\System\Utils::ExplodeIntUids((string) $Uids);
 
-		if (0 === \strlen(\trim($Folder)) || 0 === \strlen(\trim($ToFolder)) || !\is_array($aUids) || 0 === \count($aUids))
-		{
-			throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Notifications::InvalidInputParameter);
-		}
+        if (0 === \strlen(\trim($Folder)) || 0 === \strlen(\trim($ToFolder)) || !\is_array($aUids) || 0 === \count($aUids)) {
+            throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Notifications::InvalidInputParameter);
+        }
 
-		$oMailModule = \Aurora\Api::GetModule('Mail');
-		if ($oMailModule) {
-			$oAccount = $oMailModule->getAccountsManager()->getAccountById($AccountID);
-			Mail\Module::checkAccess($oAccount);
+        $oMailModule = \Aurora\Api::GetModule('Mail');
+        if ($oMailModule) {
+            $oAccount = $oMailModule->getAccountsManager()->getAccountById($AccountID);
+            Mail\Module::checkAccess($oAccount);
 
-			$oToAccount = $oMailModule->getAccountsManager()->getAccountById($ToAccountID);
-			Mail\Module::checkAccess($oToAccount);	
+            $oToAccount = $oMailModule->getAccountsManager()->getAccountById($ToAccountID);
+            Mail\Module::checkAccess($oToAccount);
 
-			try
-			{
-				$oImapClient = $oMailModule->getMailManager()->_getImapClient($oToAccount);
+            try {
+                $oImapClient = $oMailModule->getMailManager()->_getImapClient($oToAccount);
 
-				$aFlags = $oMailModule->getMailManager()->getMessagesFlags($oAccount, $Folder, $aUids);
+                $aFlags = $oMailModule->getMailManager()->getMessagesFlags($oAccount, $Folder, $aUids);
 
-				foreach ($aUids as $iUid) {
+                foreach ($aUids as $iUid) {
+                    $aAppendFlags = isset($aFlags[$iUid]) && in_array('\\seen', $aFlags[$iUid]) ? [\MailSo\Imap\Enumerations\MessageFlag::SEEN] : null;
+                    $oMailModule->getMailManager()->directMessageToStream(
+                        $oAccount,
+                        function ($rResource) use ($oImapClient, $ToFolder, $aAppendFlags) {
+                            if (\is_resource($rResource)) {
+                                $rMessageStream = \MailSo\Base\ResourceRegistry::CreateMemoryResource();
 
-					$aAppendFlags = isset($aFlags[$iUid]) && in_array('\\seen', $aFlags[$iUid]) ? [\MailSo\Imap\Enumerations\MessageFlag::SEEN] : null;
-					$oMailModule->getMailManager()->directMessageToStream($oAccount,
-					function($rResource) use ($oImapClient, $ToFolder, $aAppendFlags) {
-						if (\is_resource($rResource)) {
+                                $iMessageStreamSize = \MailSo\Base\Utils::MultipleStreamWriter(
+                                    $rResource,
+                                    array($rMessageStream),
+                                    8192,
+                                    true,
+                                    true
+                                );
 
-							$rMessageStream = \MailSo\Base\ResourceRegistry::CreateMemoryResource();
-					
-							$iMessageStreamSize = \MailSo\Base\Utils::MultipleStreamWriter(
-								$rResource, array($rMessageStream), 8192, true, true);
-					
-							if (false !== $iMessageStreamSize && is_resource($rMessageStream)) {
+                                if (false !== $iMessageStreamSize && is_resource($rMessageStream)) {
+                                    rewind($rMessageStream);
 
-								rewind($rMessageStream);
-					
-								$iNewUid = 0;
-					
-								$oImapClient->MessageAppendStream($ToFolder, $rMessageStream, $iMessageStreamSize, $aAppendFlags, $iNewUid);
-							}
-						}
-					}, $Folder, $iUid);
-				}
-				$oMailModule->getMailManager()->deleteMessage($oAccount, $Folder, $aUids);
-				$mResult = true;
-			}
-			catch (\MailSo\Imap\Exceptions\NegativeResponseException $oException)
-			{
-				$oResponse = /* @var $oResponse \MailSo\Imap\Response */ $oException->GetLastResponse();
-				throw new \Aurora\Modules\Mail\Exceptions\Exception(Mail\Enums\ErrorCodes::CannotMoveMessageQuota, $oException,
-					$oResponse instanceof \MailSo\Imap\Response ? $oResponse->Tag.' '.$oResponse->StatusOrIndex.' '.$oResponse->HumanReadable : '');
-			}
-			catch (\Exception $oException)
-			{
-				throw new \Aurora\Modules\Mail\Exceptions\Exception(Mail\Enums\ErrorCodes::CannotMoveMessage, $oException,
-					$oException->getMessage());
-			}
-	
-			return $mResult;
-		}
-	}
-	
+                                    $iNewUid = 0;
+
+                                    $oImapClient->MessageAppendStream($ToFolder, $rMessageStream, $iMessageStreamSize, $aAppendFlags, $iNewUid);
+                                }
+                            }
+                        },
+                        $Folder,
+                        $iUid
+                    );
+                }
+                $oMailModule->getMailManager()->deleteMessage($oAccount, $Folder, $aUids);
+                $mResult = true;
+            } catch (\MailSo\Imap\Exceptions\NegativeResponseException $oException) {
+                $oResponse = /* @var $oResponse \MailSo\Imap\Response */ $oException->GetLastResponse();
+                throw new \Aurora\Modules\Mail\Exceptions\Exception(
+                    Mail\Enums\ErrorCodes::CannotMoveMessageQuota,
+                    $oException,
+                    $oResponse instanceof \MailSo\Imap\Response ? $oResponse->Tag.' '.$oResponse->StatusOrIndex.' '.$oResponse->HumanReadable : ''
+                );
+            } catch (\Exception $oException) {
+                throw new \Aurora\Modules\Mail\Exceptions\Exception(
+                    Mail\Enums\ErrorCodes::CannotMoveMessage,
+                    $oException,
+                    $oException->getMessage()
+                );
+            }
+
+            return $mResult;
+        }
+    }
 }
